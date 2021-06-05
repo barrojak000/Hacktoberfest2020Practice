fc84cf140322473af9f241fb445f6b6d9ad54970
barrojak000 <58512357+barrojak000@users.noreply.github.com>
2021-06-04 22:07:38 -0700

    Add files via upload

---
+++ SumOfTwoNumbers.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b;
+	int sum;
+
+	cout << "Enter a number: ";
+	cin >> a;
+
+	cout << "Enter anohter number: ";
+	cin >> b;
+
+	sum = a + b;
+
+	cout << a << " + " << b << " = " << sum;
+
+	cout << endl;
+	return 0;
+}
